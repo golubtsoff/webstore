2150e1d25d15d47167e2c7730d0fd913e12cbb25
Evgeniy Golubtsov <e.golubtsoff@gmail.com>
2018-02-23 16:14:14 +0700

    implements of UserService is complete

--- src/main/java/service/UserServiceImpl.java
+++ src/main/java/service/UserServiceImpl.java
@@ -27,16 +27,13 @@ public class UserServiceImpl extends PersonServiceImpl implements UserService {
     }
 
     @Override
-    public long setPurchase(long itemId, int amount) throws ServiceException {
+    public long setPurchase(long itemId, int amount) {
         try (Session session = DBService.getSession()){
             Transaction transaction = session.beginTransaction();
 
             ItemDAO itemDAO = new ItemDAOImpl(session);
             Item item = itemDAO.get(itemId);
-            if (amount > item.getAmount()){
-                transaction.rollback();
-                throw new ServiceException("Not enough amount of items");
-            }
+            checkConditionPurchase(item, amount);
             Purchase purchase = new Purchase(person, item, LocalDateTime.now(), amount);
             PurchaseDAO purchaseDAO = new PurchaseDAOImpl(session);
             Long purchaseId = purchaseDAO.create(purchase);
@@ -45,6 +42,14 @@ public class UserServiceImpl extends PersonServiceImpl implements UserService {
             return purchaseId;
         } catch (HibernateException | NoResultException e) {
             throw new DBException(e);
+        }
+    }
+
+    private void checkConditionPurchase(Item item, int amount) throws ServiceException{
+        if (item == null){
+            throw new ServiceException("There is no item");
+        } else if (amount > item.getAmount()){
+            throw new ServiceException("Not enough amount of items");
         }
     }
 }
