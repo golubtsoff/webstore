03a84e8aa6d95622d783f226b5d42b751ffb6f75
Evgeniy Golubtsov <e.golubtsoff@gmail.com>
2018-02-26 21:21:31 +0700

    refactor CurrencyFormat's metod

--- src/main/java/util/CurrencyFormat.java
+++ src/main/java/util/CurrencyFormat.java
@@ -2,22 +2,21 @@ package util;
 
 import java.math.BigDecimal;
 import java.text.NumberFormat;
+import java.util.Currency;
 import java.util.Locale;
 
 /**
  * Created by Evgeniy Golubtsov on 25.02.2018.
  */
 public class CurrencyFormat {
-    private static NumberFormat numberFormatter = NumberFormat.getCurrencyInstance(Locale.US);
+    private static Locale locale = Locale.US;
+    private static NumberFormat numberFormatter = NumberFormat.getCurrencyInstance(locale);
 
     public static String format(BigDecimal number) {
         return numberFormatter.format(number);
     }
 
     public static String getCurrency(){
-        return numberFormatter.format(0)
-                .replace("0","")
-                .replace(".", "")
-                .replace(",", "");
+        return Currency.getInstance(locale).getSymbol(locale);
     }
 }
