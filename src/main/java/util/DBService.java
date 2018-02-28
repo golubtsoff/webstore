843a8e288f64480ca76b2651ad4151708f70a765
woodhead <evigol@mail.ru>
2018-02-28 17:15:18 +0700

    added database.properties

--- src/main/java/util/DBService.java
+++ src/main/java/util/DBService.java
@@ -4,11 +4,22 @@ import entity.Item;
 import entity.Person;
 import entity.Purchase;
 import entity.Role;
+import exception.ServiceException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 public class DBService {
     private static final String hibernate_show_sql = "true";
@@ -20,12 +31,17 @@ public class DBService {
 //    create-drop: drop the schema when the SessionFactory is closed explicitly, typically when the application is stopped.
     private static final String hibernate_hbm2ddl_auto = "create";
 
-    private static final SessionFactory sessionFactory;
+    private static final String filename = "database.properties";
+
+    private static SessionFactory sessionFactory;
 
     static {
-//        Configuration configuration = getH2Configuration();
-        Configuration configuration = getMySqlConfiguration();
-        sessionFactory = createSessionFactory(configuration);
+        try {
+            Configuration configuration = getConfiguration();
+            sessionFactory = createSessionFactory(configuration);
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
     }
 
     private DBService() {
@@ -62,6 +78,29 @@ public class DBService {
         configuration.setProperty("hibernate.show_sql", hibernate_show_sql);
         configuration.setProperty("hibernate.hbm2ddl.auto", hibernate_hbm2ddl_auto);
 
+        return configuration;
+    }
+
+    private static Configuration getConfiguration() throws ServiceException {
+        Configuration configuration = new Configuration();
+        addAnnotatedClassToConfiguration(configuration);
+        String path = DBService.class.getClassLoader().getResource(".").getPath() + filename;
+
+        try (InputStream is = new FileInputStream(path)) {
+            Properties props = new Properties();
+            props.load(is);
+
+            configuration.setProperty("hibernate.dialect", props.getProperty("hibernate.dialect"));
+            configuration.setProperty("hibernate.connection.driver_class", props.getProperty("hibernate.connection.driver_class"));
+            configuration.setProperty("hibernate.connection.url", props.getProperty("hibernate.connection.url"));
+            configuration.setProperty("hibernate.connection.username", props.getProperty("hibernate.connection.username"));
+            configuration.setProperty("hibernate.connection.password", props.getProperty("hibernate.connection.password"));
+            configuration.setProperty("hibernate.show_sql", props.getProperty("hibernate.show_sql"));
+            configuration.setProperty("hibernate.hbm2ddl.auto", props.getProperty("hibernate.hbm2ddl.auto"));
+
+        } catch (IOException e) {
+            throw new ServiceException("Invalid config file " + path);
+        }
         return configuration;
     }
 
